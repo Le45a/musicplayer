b8442ce36e53037d3a54fc444e2aa5376e95521e
le <2549720891@qq.com>
2020-02-24 10:01:44 +0800

    推荐部分样式完成

--- src/components/recommand.vue
+++ src/components/recommand.vue
@@ -2,15 +2,47 @@
   <div class="recommand">
     <div class="bgcolor"></div>
     <banner class="ban" />
-    <div></div>
+    <div>
+      <div class="recommand-list-container">
+        <span>推荐歌单</span>
+        <div class="recommand-list">
+          <div v-for="item in recommandList" :key="item.id" class="recommand-list-item">
+            <a :href="item.creator.avatarUrl">
+              <img :src="item.picUrl" />
+              <p>{{item.name}}</p>
+            </a>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 import Banner from './banner'
 export default {
+  data() {
+    return {
+      recommandList: []
+    }
+  },
   components: {
     Banner
+  },
+  created() {
+    this.getRecommandList()
+  },
+  methods: {
+    async getRecommandList() {
+      this.$http({
+        method: 'get',
+        url: '/recommend/resource',
+        withCredentials: true
+      }).then(res => {
+        this.recommandList = res.data.recommend
+        window.console.log(this.recommandList)
+      })
+    }
   }
 }
 </script>
@@ -28,4 +60,37 @@ export default {
   position: absolute;
   top: 0;
 }
+.recommand-list-container {
+  width: 100%;
+  margin-top: 30px;
+  .recommand-list {
+    margin-top: 20px;
+    display: flex;
+    flex-direction: row;
+    overflow-x: scroll;
+    &::-webkit-scrollbar {
+      //去除滚动条，设置滚动条的背景颜色为透明
+      background-color: transparent;
+      display: none;
+    }
+    .recommand-list-item {
+      height: 200px;
+      width: 100px;
+      margin-left: 20px;
+      a {
+        text-decoration: none;
+        color: #7a7a7a;
+      }
+      img {
+        height: 100px;
+        width: 100px;
+        border-radius: 3px;
+      }
+    }
+    .recommand-list-item:last-child{
+      padding-right: 20px;
+    }
+  }
+}
+
 </style>
